33474026c5ef4c0180e4a49158333e8a3573ecf6
Vicky-Lind <112470720+Vicky-Lind@users.noreply.github.com>
2023-09-22 09:52:25 +0300

    50% MVP, 22.09.2023

--- Pomodoro1/main.py
+++ Pomodoro1/main.py
@@ -2,11 +2,9 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QFrame, QLabel, QPushButt
 from PyQt5 import QtCore
 from PyQt5.QtCore import *
 import sys
-from PyQt5.QtGui import QCursor, QPalette, QBrush, QColor
-import time
-from PyQt5.QtGui import QPainter, QBrush, QPen, QPixmap
+from PyQt5.QtGui import QCursor
 from PyQt5.QtCore import Qt
-from qroundprogressbar import QRoundProgressBar
+from PyQt5.QtMultimedia import QSound
 
 # TODO: Add a progress bar
 # TODO: Add a settings button
@@ -17,8 +15,8 @@ from qroundprogressbar import QRoundProgressBar
 # TODO: Add a reset button
 # TODO: Add dictionary to store welcome_label texts
 
-WORK_TIME_INT =  5
-REST_TIME_INT = 2
+WORK_TIME_INT =  10
+REST_TIME_INT = 5
 
 def secs_to_minsec(secs: int):
     mins = secs // 60
@@ -62,14 +60,14 @@ class MainWindow(QMainWindow):
             }
         """)
         
-        start_button = QPushButton(self)
-        start_button.setText("Start Timer")
-        start_button.clicked.connect(self.startTimer)
-        start_button.setGeometry(39,397,227,43)
-        start_button.setCursor(QCursor(Qt.PointingHandCursor))
-        start_button.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=12, xOffset=0, yOffset=8))
+        self.start_button = QPushButton(self)
+        self.start_button.setText("Start Timer")
+        self.start_button.clicked.connect(self.startTimer)
+        self.start_button.setGeometry(39,397,227,43)
+        self.start_button.setCursor(QCursor(Qt.PointingHandCursor))
+        self.start_button.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=12, xOffset=0, yOffset=8))
         
-        start_button.setStyleSheet("""
+        self.start_button.setStyleSheet("""
             QPushButton {
                 background-color: rgba(22,202,159, 56%);
                 border-radius: 20px;
@@ -114,53 +112,30 @@ class MainWindow(QMainWindow):
                 font-weight: bold;
             }
         """)
-        # self.progress = QRoundProgressBar(self)
-        # self.progress.setBarStyle(QRoundProgressBar.BarStyle.DONUT)
-
-        # # style accordingly via palette
-        # self.palette = QPalette()
-        # brush = QBrush(QColor(0, 0, 255))
-        # brush.setStyle(Qt.SolidPattern)
-        # self.palette.setBrush(QPalette.Active, QPalette.Highlight, brush)
-
-        # self.progress.setPalette(self.palette)
-        # self.setCentralWidget(self.progress)
         
         self.time_label = QLabel(self)
         self.time_label.setAlignment(Qt.AlignCenter)  
+        self.time_label.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=0, yOffset=6))
         self.time_label.setText("01:00")
-        self.time_label.setGeometry(56,145,190,82)        
+        self.time_label.setGeometry(36,71,235,235)        
         self.time_label.setStyleSheet("""
             QLabel {
-                background: transparent;
+                background: rgba(236, 176, 198, 50%);
                 color: white;
                 font-family: arial;
                 font-size: 50.97pt;
+                border-radius: 115px;
+                border: 14px solid rgb(236, 176, 198);
             }
         """)
         self.time_label.setParent(main_frame)
+        
         
         self.time_left_int = WORK_TIME_INT
         
         self.timer = QTimer(self)
         self.update_gui()
         
-        
-        self.paint = QPainter(self)
-    
-        self.label = QLabel(self)
-        self.label.setStyleSheet
-        canvas = QPixmap(400, 300)
-        canvas.fill(Qt.white)
-        self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
-        self.draw_something()
-
-    def draw_something(self):
-        painter = QPainter(self.label.pixmap())
-        painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
-        painter.drawEllipse(40, 40, 400, 400)
-        painter.end()
     
     def startTimer(self):
         if self.WORK_TIME:
@@ -181,12 +156,14 @@ class MainWindow(QMainWindow):
                 self.welcome_label.setText("Sit back and relax 😎")
                 self.info_label_1.setText("Now: Rest")
                 self.info_label_2.setText("Up Next: Work Time")
+                QSound.play("media/ringtone.wav")
             else:
                 self.WORK_TIME = True
                 self.time_left_int = WORK_TIME_INT
                 self.welcome_label.setText("Time to focus!")
                 self.info_label_1.setText("Now: Work")
                 self.info_label_2.setText("Up Next: Rest Time")
+                QSound.play("media/ringtone.wav")
         self.update_gui()
         
     def update_gui(self):
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Pomodoro
+## -NOT READY-
  
- A PyQt5 app that has a pomodoro timer with a work- and resting timer.
+ # TimeSlice 🍅
+
+ A PyQt5- pomodoro timer app with a work- & resting time + a *gentle* ringtone ⏰.
+
+<img src="media/image.png" height="300">
+
+</br>
+
+🎵 Sound Effect by <a href="https://pixabay.com/users/universfield-28281460/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=126505">UNIVERSFIELD</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=126505">Pixabay</a>
